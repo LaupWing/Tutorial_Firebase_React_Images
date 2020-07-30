acdd5f9d3e6a0a01417ed898c19087f97f30a07e
LaupWing <loc_strict@hotmail.com>
2020-07-30 23:15:03 +0200

    uploadform

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const UploadForm = ()=>{
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+    const types = ['image/png', 'image/jpeg', 'image/jpg']
+
+    const changeHandler = e =>{
+        let selected = e.target.files[0];
+        if(selected && types.includes(selected.type)){
+            setFile(selected);
+            setError(null);
+        }else{
+            setFile(null);
+            setError('Please select an image file');
+        }
+    }
     return (
         <form>
-            <input type="file"/>
+            {console.log('rendering upploadform')}
+            <input type="file" onChange={changeHandler}/>
+            <div className="output">
+                {error && <div className={error}>{error}</div>}
+            </div>
         </form>
-    )
+    );
 }
 
-export default UploadForm
+export default UploadForm;
